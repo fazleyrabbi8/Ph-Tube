57ba338fcda7806cd47dfc769ccbb6666cb456f3
Fazley Rabbi <rabbifazley8@gmail.com>
2024-04-06 21:56:53 +0600

    navbar done

--- phTube.js
+++ phTube.js
@@ -1,8 +1,8 @@
 
 
-const loadData = () => {
+const loadData = (ContentID) => {
 
-    fetch('https://openapi.programming-hero.com/api/videos/categories')
+    fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
         .then(res => res.json())
         .then(data => console.log(data));
 
@@ -15,51 +15,111 @@ const loadData = () => {
 
 const showdata = (object) => {
 
-    const dataArray = object.data;
-    const cardHolder = document.getElementById('card-container');
+    const dataArray = object.data[0];
 
-    const card = document.createElement('div');
-    card.classList =('card w-auto card-compact  bg-base-100 ') ;
+    // console.log(dataArray)
 
-   
+    /* 
+    
+        const cardHolder = document.getElementById('card-container');
+    
+        const card = document.createElement('div');
+        card.classList =('card w-auto card-compact  bg-base-100 ') ;
+    
+       
+    
+    
+    
+        dataArray.forEach(data => {
+    
+            card.innerHTML = `
+            <figure class="shadow-xl rounded-lg ">
+            <img src="${data.thumbnail}" alt="Thumbnail" />
+            </figure>
+            
+            
+            
+            <!-- profile pic -->
+            <div class="flex gap-4 items-start my-4 ">
+                <img class="h-10 w-10 rounded-full " src="${data.authors[0].profile_picture}"
+                    alt="profile_picture">
+            
+                    
+                    <div class=" block gap-2">
+                    <h1 class="font-bold text-xl "> ${data.title}</h1>
+                    <h2 class="text-sm flex gap-2">${data.authors[0].profile_name}  <img class="h-5 w5" src="images/bluetick.png"
+                            alt="bluetick"></h2>
+                    <p> <span >${data.others.views}</span>Views</p>
+            
+                </div>
+            
+                </div>
+            
+            ` ;
+    
+    
+            cardHolder.appendChild(card) ;
+    
+        });
+    
+     */
+
+};
 
 
 
-    dataArray.forEach(data => {
 
-        card.innerHTML = `
-        <figure class="shadow-xl rounded-lg ">
-        <img src="${data.thumbnail}" alt="Thumbnail" />
-        </figure>
+// Show Category Buttons Dynamically from Api
+const NavbarContainer = document.getElementById('Navbar-button');
+
+const navbarbuttons = () => {
+
+
+
+
+    const Api = fetch('https://openapi.programming-hero.com/api/videos/categories')
+        .then(res => res.json())
+        .then(datas => datacategory(datas));
+
+
+    const datacategory = datas => {
+        const categories = datas.data;
+
+
+        console.log(categories);
+
+
+
+        categories.forEach(data => {
+
+            const navButtons = document.createElement('div');
+
+            navButtons.innerHTML = ` 
         
-        
-        
-        <!-- profile pic -->
-        <div class="flex gap-4 items-start my-4 ">
-            <img class="h-10 w-10 rounded-full " src="${data.authors[0].profile_picture}"
-                alt="profile_picture">
-        
-                
-                <div class=" block gap-2">
-                <h1 class="font-bold text-xl "> ${data.title}</h1>
-                <h2 class="text-sm flex gap-2">${data.authors[0].profile_name}  <img class="h-5 w5" src="images/bluetick.png"
-                        alt="bluetick"></h2>
-                <p> <span >${data.others.views}</span>Views</p>
-        
-            </div>
-        
-            </div>
-        
-        ` ;
+            <button onclick="sendCategory(${data.category_id})" class="btn text-sm text-white bg-red-500"> ${data.category} </button>
+    
+            `
+
+            NavbarContainer.appendChild(navButtons);
+        });
 
 
-        cardHolder.appendChild(card) ;
 
-    });
 
+
+
+    }
 
 
 };
 
 
-loadData();
+
+const sendCategory = id => {
+
+    // console.log(`Category${id} Button clicked`)
+
+}
+
+// loadData();
+navbarbuttons();
